627fda09c42d94e582e9c45231e18c4994de9138
Nicolas Garcia <playchathac@gmail.com>
2026-01-03 14:04:46 +0100

    major errors

--- app/actions/handleSearch.ts
+++ app/actions/handleSearch.ts
@@ -9,14 +9,3 @@ export async function search(formData: FormData) {
         redirect(`/search?${query.toString()}`);
     }
 }
-'use server';
-import { redirect } from 'next/navigation';
-
-export async function search(formData: FormData) {
-    const query = new URLSearchParams();
-    const search = formData.get('searchInput')?.toString();
-    if (search) {
-        query.set('q', search);
-        redirect(`/search?${query.toString()}`);
-    }
-}
--- app/ui/side-nav.tsx
+++ app/ui/side-nav.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Link from "next/link";
 import Image from "next/image";
 import { Home, Tag, ShoppingBag, Heart, MessageSquare, User, UsersRound } from "lucide-react";
@@ -25,44 +24,10 @@ export function SideNav() {
             </Link>
             <Link href="/" className=" flex flex-col items-center group">
                 <User/>
-                <p>My </p>
+                <p>My closet</p>
             </Link>
 
 
         </aside>
     )
-=======
-import Link from "next/link";
-import Image from "next/image";
-import { Home, Tag, ShoppingBag, Heart, MessageSquare, User, UsersRound } from "lucide-react";
-
-
-export function SideNav() {
-    return (
-        <aside className="bg-background-secondary flex flex-col h-lvh w-20 pt-16 top-0 z-10 gap-4 items-center">
-            <Link href="/" className=" flex flex-col items-center group">
-                <Home className="group-hover:stroke-3"/>
-                <p>Home</p>
-            </Link>
-            <Link href="/" className=" flex flex-col items-center group">
-                <MessageSquare className="group-hover:fill-foreground"/>
-                <p>Messages</p>
-            </Link>
-            <Link href="/" className=" flex flex-col items-center group">
-                <Heart/>
-                <p>Wishlist</p>
-            </Link>
-            <Link href="/" className=" flex flex-col items-center group">
-                <UsersRound/>
-                <p>Exchange</p>
-            </Link>
-            <Link href="/" className=" flex flex-col items-center group">
-                <User/>
-                <p>My </p>
-            </Link>
-
-
-        </aside>
-    )
->>>>>>> e9adbea5bd2df5b2d3e6cd10eab91836d09e8618
 }
