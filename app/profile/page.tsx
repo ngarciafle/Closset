1d174b26fd853630f989fcbe2181c1126509b2f9
Nicolas Garcia <playchathac@gmail.com>
2026-01-03 14:31:20 +0100

    side-nav style and links

--- app/profile/page.tsx
+++ app/profile/page.tsx
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 export default function ProfilePage() {
@@ -7,14 +6,4 @@ export default function ProfilePage() {
             <p className="text-7xl text-white">Profile Page</p>
         </main>
     )
-=======
-
-
-export default function ProfilePage() {
-    return (
-        <main className="pt-12 pl-24">
-            <p className="text-7xl text-white">Profile Page</p>
-        </main>
-    )
->>>>>>> e9adbea5bd2df5b2d3e6cd10eab91836d09e8618
 }
--- app/ui/side-nav.tsx
+++ app/ui/side-nav.tsx
@@ -10,19 +10,19 @@ export function SideNav() {
                 <Home className="group-hover:stroke-3"/>
                 <p className="text-xs">Home</p>
             </Link>
-            <Link href="/" className=" flex flex-col items-center group">
+            <Link href="/messages/" className=" flex flex-col items-center group">
                 <MessageSquare className="group-hover:fill-foreground"/>
                 <p className="text-xs">Messages</p>
             </Link>
-            <Link href="/" className=" flex flex-col items-center group">
+            <Link href="/wishlist" className=" flex flex-col items-center group">
                 <Heart/>
                 <p className="text-xs">Wishlist</p>
             </Link>
-            <Link href="/" className=" flex flex-col items-center group">
+            <Link href="/exchanges" className=" flex flex-col items-center group">
                 <UsersRound/>
                 <p className="text-xs">Exchanges</p>
             </Link>
-            <Link href="/" className=" flex flex-col items-center group">
+            <Link href="/profile-public" className=" flex flex-col items-center group">
                 <User/>
                 <p className="text-xs">My closet</p>
             </Link>
