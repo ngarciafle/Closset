61a0df2012116a504ae446071b4ddd01db17b46e
ngarciafle <playchathac@gmail.com>
2026-01-10 13:45:57 +0100

    better style form

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -3,13 +3,17 @@ import { signUp } from "@/app/actions/signUp";
 
 export default function SignUp() {
   return (
-    <div className="bg-background-secondary size-1/3 rounded-sm">
-      <Form action={signUp} className="flex flex-col gap-4 items-center">
+    <div className="bg-background-secondary size-1/2 rounded-sm">
+      <Form action={signUp} className="flex flex-col gap-4 items-center mt-12">
         <div className=" flex flex-row gap-2">
-          <label className="font-barlow text-xs" htmlFor="name">Name</label>
-          <input type="text" className="bg-foreground text-background" id="name" name="name"/>
-          <label className="font-barlow text-xs" htmlFor="lastName">Last Name</label>
-          <input type="text" className="bg-foreground text-background" id="lastName" name="lastName"/>
+          <div className="flex flex-col ">
+            <label className="font-barlow text-xs" htmlFor="name">Name</label>
+            <input type="text" className="bg-foreground text-background" id="name" name="name"/>
+          </div>
+          <div className="flex flex-col ">
+            <label className="font-barlow text-xs" htmlFor="lastName">Last Name</label>
+            <input type="text" className="bg-foreground text-background" id="lastName" name="lastName"/>
+          </div>
         </div>
         <div className="flex flex-col ">
           <label className="font-barlow text-xs" htmlFor="email">Email</label>
