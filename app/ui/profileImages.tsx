b1d057af42ffad61da33bc76e550760d839b4883
Nicolas Garcia <playchathac@gmail.com>
2026-02-10 09:44:22 +0100

    experimental style ring

--- app/ui/profileImages.tsx
+++ app/ui/profileImages.tsx
@@ -19,13 +19,15 @@ export default function ProfileImages(props: { userName: string, garments: any[]
     contentImages = ( // show garment images
       <div className="grid grid-cols-2 xl:grid-cols-4 md:grid-cols-3 w-full ">
       {props.garments.length == 0 ? <p>No garments available</p> : props.garments.map((garment: any) => ( 
-        <Link href={`/garment/${garment.id}`} key={garment.id} className="relative group w-full overflow-hidden block" style={{ aspectRatio: '3/4' }}> 
+        <Link href={`/garment/${garment.id}`} key={garment.id} className="ring relative group w-full overflow-hidden block" style={{ aspectRatio: '3/4' }}> {/* experimental style ring */}
+
           <Image  
             src={garment.images?.[0]} // only the first image --> build selector of main image?? 
             alt={garment.name || "Garment Image"}
             className="object-cover"
             fill
           />
+
           {/* <div className="absolute inset-0 bg-black/40 opacity-0 group-hover:opacity-100 transition-opacity duration-300 z-10"></div> */}
         </Link>
       ))}
@@ -36,9 +38,11 @@ export default function ProfileImages(props: { userName: string, garments: any[]
   return (
     <div className="pt-4 md:px-12 lg:px-32 w-full">
       <div className="w-full mt-3 flex justify-around ">
+
         <button className={
           cn("p-2 box-border border-b-2", state ? "hover:cursor-pointer border-background" : "border-foreground-500")
         } onClick={() => setState(false)}><Shirt/></button>
+        
         <button className={
           cn("p-2 box-border border-b-2", !state ? "hover:cursor-pointer border-background" : "border-foreground-500")
         } onClick={() => setState(true)}><ArrowBigUpDash/></button>
