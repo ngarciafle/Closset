89fa2b5f9cc942ac6152fba5b4754023490173d8
ngarciafle <playchathac@gmail.com>
2026-02-17 22:49:02 +0100

    working on container

--- app/ui/containerPhotos.tsx
+++ app/ui/containerPhotos.tsx
@@ -1,14 +1,17 @@
 
+import Image from "next/image";
 
 
-
-export default function ContainerPhotos(content: React.ReactNode) {
+export default function ContainerPhotos(content: String[]) {
+    let page;
     for (let i = 0; i < 10; i++) {
-        content = (
-            <div className="w-full h-64 bg-gray-300 rounded-lg mb-4">
-                {content}
-            </div>
+        let image = (
+            <></>
+            // <a className="w-full h-64 bg-gray-300 rounded-lg mb-4" href={content[i].link}>
+            //     {/* <Image src={content[i].image} alt="Photo" width={400} height={400} /> */}
+            // </a>
         )
+        page = page ? [...page, image] : [image];
     }
-    return content;
+    return page;
 }
